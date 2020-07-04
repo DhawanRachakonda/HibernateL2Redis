4b3aa5f8811a218ddaf99b76205ddb29f4ca2291
Dhawan Rachakonda <r.dhawan27@hotmail.co.uk>
2020-07-05 01:57:15 +0530

    feat: hascode and equals. cache for collections

--- src/main/java/com/hibernatel2/cache/redis/entities/Document.java
+++ src/main/java/com/hibernatel2/cache/redis/entities/Document.java
@@ -54,4 +54,41 @@ public class Document {
 	public void setUserId(Employee userId) {
 		this.userId = userId;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Document other = (Document) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (userId == null) {
+			if (other.userId != null)
+				return false;
+		} else if (!userId.equals(other.userId))
+			return false;
+		return true;
+	}
 }
--- src/main/java/com/hibernatel2/cache/redis/entities/Employee.java
+++ src/main/java/com/hibernatel2/cache/redis/entities/Employee.java
@@ -37,6 +37,7 @@ public class Employee {
 
     @OneToMany(mappedBy = "userId", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @Fetch(FetchMode.SUBSELECT)
+    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE, region = "userDocsCache")
     private List<Document> docs = new ArrayList<>();
 
 	public Long getId() {
@@ -69,5 +70,48 @@ public class Employee {
 
 	public void setDocs(List<Document> docs) {
 		this.docs = docs;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((comment == null) ? 0 : comment.hashCode());
+		result = prime * result + ((docs == null) ? 0 : docs.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (comment == null) {
+			if (other.comment != null)
+				return false;
+		} else if (!comment.equals(other.comment))
+			return false;
+		if (docs == null) {
+			if (other.docs != null)
+				return false;
+		} else if (!docs.equals(other.docs))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 }
